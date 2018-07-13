f8bcea974db6ed4789112ba0b96163ce59072a21
ousttrue <ousttrue@gmail.com>
2018-07-14 03:07:06 +0900

    Use UniJSON.IFileSystemAccessor

--- Assets/UniGLTF.Samples/Editor/GltfSchemaTests.cs
+++ Assets/UniGLTF.Samples/Editor/GltfSchemaTests.cs
@@ -8,12 +8,24 @@ namespace UniGLTF
 {
     public class GltfSchemaTests
     {
+        static IFileSystemAccessor m_schemaDir;
+        static IFileSystemAccessor SchemaDir
+        {
+            get
+            {
+                if (m_schemaDir == null)
+                {
+                    var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema");
+                    m_schemaDir = new FileSystemAccessor(path);
+                }
+                return m_schemaDir;
+            }
+        }
 
         [Test]
         public void Accessor()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/accessor.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("accessor.schema.json"));
             Assert.AreEqual("Accessor", fromSchema.Title);
             Assert.AreEqual("object", fromSchema.Type);
             Assert.AreEqual("integer", fromSchema.Properties["bufferView"].Type);
@@ -26,8 +38,7 @@ namespace UniGLTF
         [Test]
         public void AccessorSparseIndices()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/accessor.sparse.indices.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("accessor.sparse.indices.schema.json"));
             var fromClass = JsonSchema.Create<glTFSparseIndices>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -35,8 +46,7 @@ namespace UniGLTF
         [Test]
         public void AccessorSparse()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/accessor.sparse.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("accessor.sparse.schema.json"));
             var fromClass = JsonSchema.Create<glTFSparse>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -44,8 +54,7 @@ namespace UniGLTF
         [Test]
         public void AccessorSparseValues()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/accessor.sparse.values.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("accessor.sparse.values.schema.json"));
             var fromClass = JsonSchema.Create<glTFSparseValues>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -53,8 +62,7 @@ namespace UniGLTF
         [Test]
         public void AnimationChannel()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/animation.channel.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("animation.channel.schema.json"));
             var fromClass = JsonSchema.Create<glTFAnimationChannel>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -62,8 +70,7 @@ namespace UniGLTF
         [Test]
         public void AnimationChannelTarget()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/animation.channel.target.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("animation.channel.target.schema.json"));
             var fromClass = JsonSchema.Create<glTFAnimationTarget>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -71,8 +78,7 @@ namespace UniGLTF
         [Test]
         public void AnimationSampler()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/animation.sampler.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("animation.sampler.schema.json"));
             var fromClass = JsonSchema.Create<glTFAnimationSampler>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -80,8 +86,7 @@ namespace UniGLTF
         [Test]
         public void Animation()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/animation.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("animation.schema.json"));
             var fromClass = JsonSchema.Create<glTFAnimation>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -89,8 +94,7 @@ namespace UniGLTF
         [Test]
         public void Asset()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/asset.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("asset.schema.json"));
             var fromClass = JsonSchema.Create<glTFAssets>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -98,8 +102,7 @@ namespace UniGLTF
         [Test]
         public void Buffer()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/buffer.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("buffer.schema.json"));
             var fromClass = JsonSchema.Create<glTFBuffer>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -107,8 +110,7 @@ namespace UniGLTF
         [Test]
         public void BufferView()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/bufferView.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("bufferView.schema.json"));
             var fromClass = JsonSchema.Create<glTFBufferView>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -116,8 +118,7 @@ namespace UniGLTF
         [Test]
         public void Gltf()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/glTF.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("glTF.schema.json"));
             var fromClass = JsonSchema.Create<glTF>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -125,8 +126,7 @@ namespace UniGLTF
         [Test]
         public void Image()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/image.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("image.schema.json"));
             var fromClass = JsonSchema.Create<glTFImage>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -134,8 +134,7 @@ namespace UniGLTF
         [Test]
         public void MaterialNormalTextureInfo()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/material.normalTextureInfo.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("material.normalTextureInfo.schema.json"));
             var fromClass = JsonSchema.Create<glTFMaterialNormalTextureInfo>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -143,8 +142,7 @@ namespace UniGLTF
         [Test]
         public void MaterialOcclusionTextureInfo()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/material.occlusionTextureInfo.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("material.occlusionTextureInfo.schema.json"));
             var fromClass = JsonSchema.Create<glTFMaterialOcclusionTextureInfo>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -152,8 +150,7 @@ namespace UniGLTF
         [Test]
         public void MaterialPbrMetallicRoughness()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/material.pbrMetallicRoughness.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("material.pbrMetallicRoughness.schema.json"));
             var fromClass = JsonSchema.Create<glTFPbrMetallicRoughness>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -161,8 +158,7 @@ namespace UniGLTF
         [Test]
         public void Material()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/material.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("material.schema.json"));
             var fromClass = JsonSchema.Create<glTFMaterial>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -170,8 +166,7 @@ namespace UniGLTF
         [Test]
         public void MeshPrimitive()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/mesh.primitive.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("mesh.primitive.schema.json"));
             var fromClass = JsonSchema.Create<glTFPrimitives>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -179,8 +174,7 @@ namespace UniGLTF
         [Test]
         public void Mesh()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/mesh.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("mesh.schema.json"));
             var fromClass = JsonSchema.Create<glTFMesh>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -188,8 +182,7 @@ namespace UniGLTF
         [Test]
         public void Node()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/node.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("node.schema.json"));
             var fromClass = JsonSchema.Create<glTFNode>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -197,8 +190,7 @@ namespace UniGLTF
         [Test]
         public void Sampler()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/sampler.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("sampler.schema.json"));
             var fromClass = JsonSchema.Create<glTFTextureSampler>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -206,8 +198,7 @@ namespace UniGLTF
         [Test]
         public void Scene()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/scene.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("scene.schema.json"));
             var fromClass = JsonSchema.Create<gltfScene>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -215,8 +206,7 @@ namespace UniGLTF
         [Test]
         public void Skin()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/skin.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("skin.schema.json"));
             var fromClass = JsonSchema.Create<glTFSkin>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -224,8 +214,7 @@ namespace UniGLTF
         [Test]
         public void Texture()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/texture.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("texture.schema.json"));
             var fromClass = JsonSchema.Create<glTFTexture>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
@@ -233,8 +222,7 @@ namespace UniGLTF
         [Test]
         public void TextureInfo()
         {
-            var path = Path.GetFullPath(Application.dataPath + "/../glTF/specification/2.0/schema/textureInfo.schema.json");
-            var fromSchema = JsonSchema.ParseFromPath(path);
+            var fromSchema = JsonSchema.ParseFromPath(SchemaDir.GetRelative("textureInfo.schema.json"));
             var fromClass = JsonSchema.Create<glTFTextureInfo>();
             Assert.True(fromSchema.MatchProperties(fromClass));
         }
